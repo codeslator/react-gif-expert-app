d69aee1ce162c22d507d234517f4e6f44203aebc
Andrés Meléndez <codeslator@gmail.com>
2024-07-15 19:27:03 -0400

    Changes: Create folder structure with barrel files;

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api';
 import GifExpertApp from './GifExpertApp.tsx'
 import "primereact/resources/themes/lara-dark-teal/theme.css";
-import './index.css'
+import './views/assets/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
