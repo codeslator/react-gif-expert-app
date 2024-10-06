5f5de3647385aae635157054834a8c823c5dda85
Andrés Meléndez <codeslator@gmail.com>
2024-10-06 16:45:33 -0400

    Changes: Fix problem with css parsers and add mock to use meta.import from Vite; Add test suite with cases for GifItem component;

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,27 @@
 export default {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": ["ts-jest", {
+      diagnostics: {
+        ignoreCodes: [1343],
+      },
+      astTransformers: {
+        before: [
+          {
+            path: 'node_modules/ts-jest-mock-import-meta',
+            options: {
+              metaObjectReplacement: {
+                env: {
+                  VITE_GIPHY_API_URL: 'https://api.giphy.com/v1/gifs/search',
+                  VITE_GIPHY_API_KEY: 'your api key here',
+                  VITE_GITHUB_REPO_URL: 'https://github.com/codeslator/gif-expert-app'
+                },
+              },
+            },
+          },
+        ],
+      },
+    },]
   },
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
---
+++ src/tests/GifItem.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react"
+import { GifItem } from "../views/pages/components"
+
+describe('Test <GifItem /> component.', () => {
+  const gif = {
+    title: 'Pokemon',
+    url: 'https://pokemon.com/bulbasaur.jpg',
+    id: "abc123"
+  }
+  
+  test('Should to match with the snapshot.', () => {
+    const { container } = render(<GifItem gif={gif} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should display alt and url correctly.', () => {
+    render(<GifItem gif={gif} />);
+    // expect(screen.getByRole<HTMLImageElement>('img').src).toBe(gif.url);
+    const { src, alt } = screen.getByRole<HTMLImageElement>('img');
+    expect(src).toBe(gif.url);
+    expect(alt).toBe(gif.title);
+  });
+
+  test('Should display the title in the component.', () => {
+    render(<GifItem gif={gif} />);
+    expect(screen.getByText(gif.title)).toBeTruthy();
+  })
+})
